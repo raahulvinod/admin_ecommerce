ee38dcabba667892453a49512e979c2e69292d41
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-09-05 22:23:32 +0530

    Alert modal added

--- admin-dashboard/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ admin-dashboard/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,11 +1,14 @@
 'use client';
 
+import * as z from 'zod';
+import axios from 'axios';
 import { useState } from 'react';
 import { Store } from '@prisma/client';
 import { Trash } from 'lucide-react';
-import * as z from 'zod';
 import { useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useParams, useRouter } from 'next/navigation';
 
 import { Button } from '@/components/ui/button';
 import { Heading } from '@/components/ui/heading';
@@ -19,6 +22,7 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { AlertModal } from '@/components/modals/alert-modal';
 
 interface SettingsFormProps {
   initialData: Store;
@@ -31,6 +35,9 @@ const formSchema = z.object({
 type SettingFormValues = z.infer<typeof formSchema>;
 
 export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const params = useParams();
+  const router = useRouter();
+
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
@@ -40,11 +47,26 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   });
 
   const onSubmit = async (data: SettingFormValues) => {
-    console.log(data);
+    try {
+      setLoading(true);
+      await axios.patch(`/api/stores/${params.storeId}`, data);
+      router.refresh();
+      toast.success('Store updated.');
+    } catch (error) {
+      toast.error('Something went wrong.');
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={() => {}}
+        loading={loading}
+      />
       <div className="flex items-center justify-between">
         <Heading title="Settings" description="Manage store preferences" />
         <Button
