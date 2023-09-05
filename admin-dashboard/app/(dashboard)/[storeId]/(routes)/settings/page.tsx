e66c5a941974022a80aa95b157f4f69d7523ccdc
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-09-05 21:25:33 +0530

    Store settings form validation

--- admin-dashboard/app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ admin-dashboard/app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -2,6 +2,8 @@ import prismadb from '@/lib/prismadb';
 import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 
+import { SettingsForm } from './components/settings-form';
+
 interface SettingsPageProps {
   params: { storeId: string };
 }
@@ -25,7 +27,9 @@ const SettingsPage: React.FC<SettingsPageProps> = async ({ params }) => {
   }
   return (
     <div className="flex-col">
-      <div className="flex-1 space-y-4 p-8 pt-6">Settings Page</div>
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SettingsForm initialData={store} />
+      </div>
     </div>
   );
 };
