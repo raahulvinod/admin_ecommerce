531ba054d9c867336aa1a34e35fc57c1d9040876
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-09-11 00:27:08 +0530

    Billboard banner added

--- store/app/(routes)/page.tsx
+++ store/app/(routes)/page.tsx
@@ -1,5 +1,22 @@
-const HomePage = () => {
-  return <div>Hello store</div>;
+import getBillboard from '@/actions/get-billboard';
+import getProducts from '@/actions/get-products';
+import Billboard from '@/components/billboard';
+import ProductList from '@/components/product-list';
+import Container from '@/components/ui/container';
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard('3da1cb72-0e33-4f37-8fee-3708b9439901');
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
