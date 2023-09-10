ea501ae63c4f3690308d035eaefd43ce6b5ed56b
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-09-10 19:28:39 +0530

    woriking with main nav

--- store/components/navbar.tsx
+++ store/components/navbar.tsx
@@ -1,5 +1,20 @@
+import Container from '@/components/ui/container';
+import Link from 'next/link';
+import MainNav from './main-nav';
+
 const Navbar = () => {
-  return <div className="border-b">navbar</div>;
+  return (
+    <div className="border-b">
+      <Container>
+        <div className="relative px-4 sm:px-6 lg:px-8 h-16 flex items-center">
+          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+            <p className="font-bold text-xl">Brandistic</p>
+          </Link>
+          <MainNav data={[]} />
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Navbar;
