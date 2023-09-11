46d68a7660968e56fbc3cd97389e9db0f88fbb87
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-09-11 14:44:44 +0530

    Gallery section - single product image

--- store/app/(routes)/product/[productId]/page.tsx
+++ store/app/(routes)/product/[productId]/page.tsx
@@ -1,7 +1,9 @@
 import getProduct from '@/actions/get-product';
 import getProducts from '@/actions/get-products';
+import Gallery from '@/components/gallery';
 import ProductList from '@/components/product-list';
 import Container from '@/components/ui/container';
+
 
 interface ProductPageProps {
   params: {
@@ -19,7 +21,7 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
       <Container>
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-            <div>Gallery</div>
+            <Gallery images={product.images} />
             <div className="mt-10 px-4 sm:px-0 lg:mt-0">Info</div>
           </div>
           <hr className="mt-10" />
